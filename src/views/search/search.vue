5d15b2d1d50fb4b718690ff3f8c4fefeba03ae54
MangoIsGreat <51810159+MangoIsGreat@users.noreply.github.com>
2020-01-04 23:30:58 +0800

    搜索-静态页面-完成搜索历史和联想区域

--- src/views/search/search.vue
+++ src/views/search/search.vue
@@ -1,15 +1,46 @@
 <template>
   <div>
+    <!-- 搜索框区域 -->
     <van-search
-        v-model="value"
-        placeholder="请输入搜索关键词"
-        show-action
-        background="#3296fa"
-        @search="onSearch">
-            <template slot="action">
-                <div class="mycancel">取消</div>
-            </template>
+      v-model="value"
+      placeholder="请输入搜索关键词"
+      show-action
+      background="#3296fa"
+      @search="onSearch"
+    >
+      <template slot="action">
+        <div class="mycancel">取消</div>
+      </template>
     </van-search>
+    <!-- 联想区域 -->
+    <van-cell-group v-if="isThink">
+      <van-cell icon="search" title="程序员为什么那么帅" />
+      <van-cell icon="search" title="程序员为什么高薪" />
+      <van-cell icon="search" title="程序员为什么喜欢穿格子衫" />
+    </van-cell-group>
+    <!-- 历史区域 -->
+    <van-cell-group v-else>
+      <van-cell title="搜索历史">
+          <template slot="default">
+              <van-icon name="delete" />
+          </template>
+      </van-cell>
+      <van-cell  icon="search" title="黑马吴彦祖">
+          <template slot="right-icon">
+              <van-icon name="cross" />
+          </template>
+      </van-cell>
+      <van-cell  icon="search" title="鸡冻">
+          <template slot="right-icon">
+              <van-icon name="cross" />
+          </template>
+      </van-cell>
+      <van-cell  icon="search" title="java">
+          <template slot="right-icon">
+              <van-icon name="cross" />
+          </template>
+      </van-cell>
+    </van-cell-group>
   </div>
 </template>
 
@@ -18,7 +49,9 @@ export default {
   name: 'search',
   data () {
     return {
-      value: ''
+      value: '',
+      //   控制联想区域和历史区域的显示和隐藏
+      isThink: false
     }
   },
   methods: {
@@ -34,14 +67,14 @@ export default {
 
 <style lang="less">
 .mycancel {
-    color: #fff;
+  color: #fff;
 }
 
 .mycancel:active {
-    color: #ccc;
+  color: #ccc;
 }
 
 .van-search__action:active {
-    background-color: #3296fa;
+  background-color: #3296fa;
 }
 </style>
