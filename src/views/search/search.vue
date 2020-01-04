19d9478d53f528836bdd128355d631752bf8a6c0
MangoIsGreat <51810159+MangoIsGreat@users.noreply.github.com>
2020-01-04 23:07:23 +0800

    搜索-静态页面-完成搜索区域

--- src/views/search/search.vue
+++ src/views/search/search.vue
@@ -1,15 +1,47 @@
 <template>
   <div>
-      这是搜索页面
+    <van-search
+        v-model="value"
+        placeholder="请输入搜索关键词"
+        show-action
+        background="#3296fa"
+        @search="onSearch">
+            <template slot="action">
+                <div class="mycancel">取消</div>
+            </template>
+    </van-search>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'search'
+  name: 'search',
+  data () {
+    return {
+      value: ''
+    }
+  },
+  methods: {
+    onSearch (key) {
+      window.console.log(key)
+    },
+    onCancel () {
+      window.console.log('oncancel')
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="less">
+.mycancel {
+    color: #fff;
+}
 
+.mycancel:active {
+    color: #ccc;
+}
+
+.van-search__action:active {
+    background-color: #3296fa;
+}
 </style>
