c88d8e7353709244016c8f7d8631b2192cbc9dfd
MangoIsGreat <51810159+MangoIsGreat@users.noreply.github.com>
2020-01-06 00:44:42 +0800

    完成作者信息&文章&点赞按钮

--- src/views/details/index.vue
+++ src/views/details/index.vue
@@ -2,7 +2,37 @@
   <div class="detailsBox">
       <van-nav-bar @click-left="$router.back()" left-arrow fixed title="文章详情" />
       <!-- 文章标题 -->
-      <h1 class="detailTitle">this的指向问题</h1>
+      <h2 class="detailTitle">this的指向问题</h2>
+      <!-- 作者详情： -->
+      <van-cell class="autInfo">
+        <template slot="title">
+          <div class="aut-title">
+            <div class="autImg">
+              <img src="http://img5.imgtn.bdimg.com/it/u=389433887,3486657666&fm=26&gp=0.jpg" alt="">
+            </div>
+            <div class="autMsg">
+              <div>作者名称</div>
+              <div>发布时间</div>
+            </div>
+            <div class="followed">
+              <van-button type="info">+ 关注</van-button>
+            </div>
+          </div>
+        </template>
+      </van-cell>
+      <!-- 文章详情： -->
+      <van-cell>
+        <div v-for="(item, index) in 10" :key="index">内容区域</div>
+      </van-cell>
+      <!-- 点赞&不喜欢 -->
+      <van-cell class="like">
+        <van-button plain hairline round class="checkLike">
+          <van-icon class="like-icon" name="like" />&nbsp;&nbsp;&nbsp;&nbsp;点赞
+        </van-button>
+        <van-button plain hairline round class="dislike">
+          <van-icon class="like-icon" name="delete" />&nbsp;&nbsp;&nbsp;&nbsp;不喜欢
+        </van-button>
+      </van-cell>
   </div>
 </template>
 
@@ -13,6 +43,10 @@ export default {
 </script>
 
 <style lang="less">
+body {
+  background-color: #fff;
+}
+
 .detailsBox {
   padding-top: 46px;
 
@@ -24,6 +58,43 @@ export default {
     font-weight: 400;
     line-height: 50px;
     text-align: center;
+  }
+
+  .autInfo {
+    .aut-title {
+      display: flex;
+
+      .autImg {
+        img {
+          width: 50px;
+          height: 50px;
+          border-radius: 50%;
+        }
+      }
+
+      .autMsg {
+        flex: 1;
+        margin-left: 15px;
+      }
+    }
+  }
+
+  .like {
+    .van-cell__value {
+      display: flex;
+      justify-content: space-around;
+    }
+
+    .checkLike {
+      padding: 0 26px;
+      border-color: #444;
+    }
+
+    .dislike {
+      padding: 0 26px;
+      border-color: red;
+      color: red;
+    }
   }
 }
 </style>
