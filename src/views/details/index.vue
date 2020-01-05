7f4fbdf413f8238dd581e0f967fe5eb649057c00
MangoIsGreat <51810159+MangoIsGreat@users.noreply.github.com>
2020-01-06 00:03:22 +0800

    详情页面-完成顶部标题和文章标题区域

--- src/views/details/index.vue
+++ src/views/details/index.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-      <h1>这是details页面</h1>
+  <div class="detailsBox">
+      <van-nav-bar @click-left="$router.back()" left-arrow fixed title="文章详情" />
+      <!-- 文章标题 -->
+      <h1 class="detailTitle">this的指向问题</h1>
   </div>
 </template>
 
@@ -10,6 +12,18 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less">
+.detailsBox {
+  padding-top: 46px;
 
+  .van-nav-bar .van-icon {
+    color: #fff;
+  }
+
+  .detailTitle {
+    font-weight: 400;
+    line-height: 50px;
+    text-align: center;
+  }
+}
 </style>
