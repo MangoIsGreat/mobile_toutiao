2fa4a160d863c7c46d79506c15c4bcae518c7c8b
MangoIsGreat <51810159+MangoIsGreat@users.noreply.github.com>
2020-01-07 17:27:16 +0800

    个人信息页面-静态页面完成

--- src/views/layout/layout.vue
+++ src/views/layout/layout.vue
@@ -1,18 +1,23 @@
 <template>
   <div>
     <router-view></router-view>
-    <van-tabbar router>
+    <van-tabbar router v-model="active">
       <van-tabbar-item to="/home" icon="home-o">首页</van-tabbar-item>
       <van-tabbar-item icon="search" to="/search">搜索</van-tabbar-item>
-      <van-tabbar-item icon="friends-o">视频</van-tabbar-item>
-      <van-tabbar-item to="/my" icon="setting-o">我的</van-tabbar-item>
+      <van-tabbar-item icon="video-o">视频</van-tabbar-item>
+      <van-tabbar-item to="/my" icon="user-circle-o">我的</van-tabbar-item>
     </van-tabbar>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'layout'
+  name: 'layout',
+  data () {
+    return {
+      active: 0
+    }
+  }
 }
 </script>
 
--- src/views/my/my.vue
+++ src/views/my/my.vue
@@ -1,5 +1,47 @@
 <template>
-  <van-nav-bar title="我的" />
+  <div class="user">
+    <!-- 用户个人信息区域: -->
+    <div class="myInfo">
+      <div class="userInfo">
+        <div class="uImg">
+          <img src="https://f10.baidu.com/it/u=1900326330,1437211603&fm=72" alt="">
+        </div>
+        <div class="uName">朱义龙</div>
+        <div class="today">
+          <div>今日阅读</div>
+          <div>1分钟</div>
+        </div>
+      </div>
+      <div class="activity">
+        <div>
+          <div>620000</div>
+          <div>动态</div>
+        </div>
+        <div>
+          <div>25</div>
+          <div>关注</div>
+        </div>
+        <div>
+          <div>169</div>
+          <div>粉丝</div>
+        </div>
+      </div>
+    </div>
+    <!-- 收藏区域： -->
+    <div class="collect">
+      <van-grid :column-num="3">
+        <van-grid-item class="collection" icon="star-o" text="收藏" />
+        <van-grid-item class="history" icon="clock-o" text="历史" />
+        <van-grid-item class="production" icon="records" text="作品" />
+      </van-grid>
+    </div>
+    <!-- 消息通知区域： -->
+    <div class="message">
+      <van-cell title="消息通知" is-link />
+      <van-cell title="用户反馈" is-link />
+      <van-cell title="小智同学" is-link />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +50,68 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.user {
+  background-color: rgb(244, 245, 248);
+  .myInfo {
+    padding: 30px 20px;
+    background-color: rgb(61, 155, 246);
+    .userInfo {
+      display: flex;
+      position: relative;
+      .uImg {
+        margin: 0 20px;
+        img {
+          width: 60px;
+          height: 60px;
+          border-radius: 50%;
+        }
+      }
+      .uName {
+        color: #fff;
+        font-size: 20px;
+      }
+      .today {
+        position: absolute;
+        right: -20px;
+        top: 0;
+        background-color: rgba(0, 0, 0, .4);
+        border-radius: 50px 0 0 50px;
+        padding: 5px 14px;
+        text-align: center;
+        div {
+          color: #fff;
+          font-size: 12px;
+          line-height: 18px;
+        }
+      }
+    }
 
+    .activity {
+      margin-top: 20px;
+      display: flex;
+      text-align: center;
+      div {
+        flex: 1;
+        color: #fff;
+      }
+    }
+  }
+
+  .collect {
+    .collection {
+      color: rgb(233, 112, 116);
+    }
+    .history {
+      color: rgb(231, 170, 83);
+    }
+    .production {
+      color: rgb(139, 163, 225);
+    }
+  }
+
+  .message {
+    margin-top: 10px;
+  }
+}
 </style>
